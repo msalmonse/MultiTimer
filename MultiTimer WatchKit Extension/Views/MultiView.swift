16c5cb482b6b4d737f428ecc50e9e46ff29c6aef
Michael Salmon <msalmonse@gmail.com>
2019-08-26 11:47:51 +0200

    View for multiple timers

---
+++ MultiTimer WatchKit Extension/Views/MultiView.swift
@@ -0,0 +1,21 @@
+//
+//  MultiView.swift
+//  MultiTimer WatchKit Extension
+//
+//  Created by Michael Salmon on 2019-08-26.
+//  Copyright © 2019 mesme. All rights reserved.
+//
+
+import SwiftUI
+
+struct MultiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MultiView_Previews: PreviewProvider {
+    static var previews: some View {
+        MultiView()
+    }
+}
